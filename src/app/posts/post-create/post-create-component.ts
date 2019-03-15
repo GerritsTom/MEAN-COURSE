87057ab458be8976db389beaca72e051bd29b7b9
Tom Gerrits <t.gerrits@christ.de>
2019-03-15 07:44:21 +0100

    18. Getting User Input

--- src/app/posts/post-create/post-create-component.ts
+++ src/app/posts/post-create/post-create-component.ts
@@ -5,10 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './post-create-component.html'
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
 
-newPost = 'NO CONTENT';
+  /* property binding and string interpolation
+  onAddPost(postInput: HTMLTextAreaElement) {
+    console.log(postInput);
+    this.newPost = postInput.value;
+  } */
 
+  /* two way binding */
   onAddPost() {
-    this.newPost = 'The user\'s post';
+    this.newPost = this.enteredValue;
   }
 }
