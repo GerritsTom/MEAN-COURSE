c530a78ebab468f973eed0cd68870c172464f808
Tom Gerrits <t.gerrits@christ.de>
2019-03-20 08:11:47 +0100

    Lektionen bis 41

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import {Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -8,8 +9,16 @@ export class PostsService {
   // Subject is a kind of eventemitter
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    // return [...this.posts];
+
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+          this.posts = postData.posts;
+          this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -17,9 +26,17 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
-    this.posts.push(post);
-    // emit the update postsarray
-    this.postsUpdated.next([...this.posts]);
+    const post: Post = {id: null, title: title, content: content};
+
+    // save on server
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+
+        this.posts.push(post);
+        // emit the update postsarray
+        this.postsUpdated.next([...this.posts]);
+      });
+
   }
 }
